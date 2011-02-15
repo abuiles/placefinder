14225893f90a221770f60a195829a872dd9d891a
Adolfo Builes <builes.adolfo@googlemail.com>
2011-02-14 23:48:37 -0500

    Adding get and done.

--- lib/placefinder/base.rb
+++ lib/placefinder/base.rb
@@ -1,4 +1,20 @@
+require 'rubygems'
+require 'httparty'
+
 module Placefinder
   class Base
+    include HTTParty
+    base_uri 'http://where.yahooapis.com/geocode'
+
+    def initialize(params = {})
+      @api_key = params[:api_key]
+    end
+
+    def get(params = {})
+      params.merge!({:appid => @api_key}) if @apy_key
+      response = self.class.get('', :query => params)
+      response.parsed_response
+    end
   end
+
 end
