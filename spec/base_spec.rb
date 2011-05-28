c08cb33e2b98aaa8a6317008235179d5f848de12
Adolfo Builes <builes.adolfo@googlemail.com>
2011-05-28 10:59:51 -0500

    adding some test mostly expectations

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -1,11 +1,27 @@
 require 'spec_helper'
 
 describe Placefinder::Base do
-  it "should assert true" do
-    tatft = "should we test all the fucking time?"
-    def tatft.should_we?
-      false
-    end
-    tatft.should_we?.should be(false)
+  API_URI = 'http://where.yahooapis.com/geocode'
+
+  before(:each) do
+    nil.stub(:parsed_response)
+  end
+
+  it "should include the API key in the request" do
+    placefinder = Placefinder::Base.new(:api_key => '123456')
+    params = {:q => "Central Park, NY, US", :flags => 'J'}
+    with_key = params.merge(:appid => '123456')
+
+    HTTParty.should_receive(:get).with(API_URI, :query => with_key)
+    placefinder.get params
+  end
+
+
+  it "should not include the api key if not given" do
+    placefinder = Placefinder::Base.new
+    params = {:q => "Central Park, NY, US", :flags => 'J'}
+
+    HTTParty.should_receive(:get).with(API_URI, :query => params)
+    placefinder.get params
   end
 end
